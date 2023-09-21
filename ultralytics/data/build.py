f0cc93588e68034ae96dafbd9a1cd57973baf6f7
Xiangjing-research <a1217128095@gmail.com>
2023-09-21 20:44:58 +0800

    添加完善build_multimodal_dataset方法

--- ultralytics/data/build.py
+++ ultralytics/data/build.py
@@ -15,7 +15,7 @@ from ultralytics.data.utils import IMG_FORMATS, VID_FORMATS
 from ultralytics.utils import RANK, colorstr
 from ultralytics.utils.checks import check_file
 
-from .dataset import YOLODataset
+from .dataset import YOLODataset, MultiModalDataset
 from .utils import PIN_MEMORY
 
 
@@ -68,9 +68,27 @@ def seed_worker(worker_id):  # noqa
     np.random.seed(worker_seed)
     random.seed(worker_seed)
 
-# def build_multimodal_dataset(cfg,rgb_path,ir_path,batch,data, mode='train', rect=False, stride=32):
 
-
+def build_multimodal_dataset(cfg, rgb_path, ir_path, batch, data, mode='train', rect=False, stride=32):
+    """Build MultiModal Dataset"""
+    return MultiModalDataset(
+        rgb_path=rgb_path,
+        ir_path=ir_path,
+        imgsz=cfg.imgsz,
+        batch_size=batch,
+        augment=mode == 'train',  # augmentation
+        hyp=cfg,  # TODO: probably add a get_hyps_from_cfg function
+        rect=cfg.rect or rect,  # rectangular batches
+        cache=cfg.cache or None,
+        single_cls=cfg.single_cls or False,
+        stride=int(stride),
+        pad=0.0 if mode == 'train' else 0.5,
+        prefix=colorstr(f'{mode}: '),
+        use_segments=cfg.task == 'segment',
+        use_keypoints=cfg.task == 'pose',
+        classes=cfg.classes,
+        data=data,
+        fraction=cfg.fraction if mode == 'train' else 1.0)
 
 
 def build_yolo_dataset(cfg, img_path, batch, data, mode='train', rect=False, stride=32):
