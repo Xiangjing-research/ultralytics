9a7cc6a24f5d9f2b4c02498d639c59d12cd14965
Xiangjing-research <a1217128095@gmail.com>
2023-09-21 20:44:07 +0800

    添加 MultiModalDetectionValidator,完善build_dataset,get_dataloader,重写__call__方法

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -1,21 +1,23 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
-
+import json
 import os
 from pathlib import Path
 
 import numpy as np
 import torch
 
-from ultralytics.data.dataset import MultiModalDataset
+from ultralytics.data.build import build_multimodal_dataset
 from ultralytics.data.utils import check_det_rgb_ir_dataset
 from ultralytics.data import build_dataloader, build_yolo_dataset, converter
 from ultralytics.engine.validator import BaseValidator
-from ultralytics.utils import DEFAULT_CFG, LOGGER, ops, colorstr
+from ultralytics.nn.autobackend import AutoBackend
+from ultralytics.utils import DEFAULT_CFG, LOGGER, ops, colorstr, emojis, callbacks, TQDM
 from ultralytics.utils import LOGGER, ops
-from ultralytics.utils.checks import check_requirements
+from ultralytics.utils.checks import check_requirements, check_imgsz
 from ultralytics.utils.metrics import ConfusionMatrix, DetMetrics, box_iou
+from ultralytics.utils.ops import Profile
 from ultralytics.utils.plotting import output_to_target, plot_images
-from ultralytics.utils.torch_utils import de_parallel
+from ultralytics.utils.torch_utils import de_parallel, select_device, smart_inference_mode
 
 
 class DetectionValidator(BaseValidator):
@@ -271,6 +273,149 @@ class DetectionValidator(BaseValidator):
         return stats
 
 
+class MultiModalDetectionValidator(DetectionValidator):
+    """
+    A class extending the BaseValidator class for validation based on a detection model.
+
+    Example:
+        ```python
+        from ultralytics.models.yolo.detect import DetectionValidator
+
+        args = dict(model='yolov8n.pt', data='coco8.yaml')
+        validator = DetectionValidator(args=args)
+        validator()
+        ```
+    """
+
+    def __init__(self, dataloader=None, save_dir=None, pbar=None, args=None, _callbacks=None):
+        """Initialize detection model with necessary variables and settings."""
+        super().__init__(dataloader, save_dir, pbar, args, _callbacks)
+
+    def build_dataset(self, rgb_path, ir_path, mode='val', batch=None):
+        """
+        Build MultiModal Dataset.
+
+        Args:
+            rgb_path (str): Path to the folder containing images.
+            ir_path (str): Path to the folder containing images.
+            mode (str): `train` mode or `val` mode, users are able to customize different augmentations for each mode.
+            batch (int, optional): Size of batches, this is for `rect`. Defaults to None.
+        """
+        gs = max(int(de_parallel(self.model).stride if self.model else 0), 32)
+        return build_multimodal_dataset(self.args, rgb_path, ir_path, batch, self.data, mode=mode, stride=gs)
+
+    def get_dataloader(self, rgb_path, ir_path, batch_size):
+        """Construct and return dataloader."""
+        dataset = self.build_dataset(rgb_path, ir_path, batch=batch_size, mode='val')
+        return build_dataloader(dataset, batch_size, self.args.workers, shuffle=False, rank=-1)  # return dataloader
+
+    @smart_inference_mode()
+    def __call__(self, trainer=None, model=None):
+        """
+        Supports validation of a pre-trained model if passed or a model being trained if trainer is passed (trainer
+        gets priority).
+        """
+        self.training = trainer is not None
+        augment = self.args.augment and (not self.training)
+        if self.training:
+            self.device = trainer.device
+            self.data = trainer.data
+            self.args.half = self.device.type != 'cpu'  # force FP16 val during training
+            model = trainer.ema.ema or trainer.model
+            model = model.half() if self.args.half else model.float()
+            # self.model = model
+            self.loss = torch.zeros_like(trainer.loss_items, device=trainer.device)
+            self.args.plots &= trainer.stopper.possible_stop or (trainer.epoch == trainer.epochs - 1)
+            model.eval()
+        else:
+            callbacks.add_integration_callbacks(self)
+            self.run_callbacks('on_val_start')
+            model = AutoBackend(model or self.args.model,
+                                device=select_device(self.args.device, self.args.batch),
+                                dnn=self.args.dnn,
+                                data=self.args.data,
+                                fp16=self.args.half)
+            # self.model = model
+            self.device = model.device  # update device
+            self.args.half = model.fp16  # update half
+            stride, pt, jit, engine = model.stride, model.pt, model.jit, model.engine
+            imgsz = check_imgsz(self.args.imgsz, stride=stride)
+            if engine:
+                self.args.batch = model.batch_size
+            elif not pt and not jit:
+                self.args.batch = 1  # export.py models default to batch-size 1
+                LOGGER.info(f'Forcing batch=1 square inference (1,3,{imgsz},{imgsz}) for non-PyTorch models')
+            if isinstance(self.args.data, str) and self.args.data.split('.')[-1] in ('yaml', 'yml'):
+                self.data = check_det_rgb_ir_dataset(self.args.data)
+            # elif self.args.task == 'classify':
+            #     self.data = check_det_rgb_ir_dataset(self.args.data, split=self.args.split)
+            else:
+                raise FileNotFoundError(emojis(f"Dataset '{self.args.data}' for task={self.args.task} not found ❌"))
+
+            if self.device.type in ('cpu', 'mps'):
+                self.args.workers = 0  # faster CPU val as time dominated by inference, not dataloading
+            if not pt:
+                self.args.rect = False
+            self.dataloader = self.dataloader or self.get_dataloader(self.data.get(self.args.split)[0],
+                                                                     self.data.get(self.args.split)[1], self.args.batch)
+
+            model.eval()
+            model.warmup(imgsz=(1 if pt else self.args.batch, 3, imgsz, imgsz))  # warmup
+
+        dt = Profile(), Profile(), Profile(), Profile()
+        bar = TQDM(self.dataloader, desc=self.get_desc(), total=len(self.dataloader))
+        self.init_metrics(de_parallel(model))
+        self.jdict = []  # empty before each val
+        for batch_i, batch in enumerate(bar):
+            self.run_callbacks('on_val_batch_start')
+            self.batch_i = batch_i
+            # Preprocess
+            with dt[0]:
+                batch = self.preprocess(batch)
+
+            # Inference
+            with dt[1]:
+                rgn_img, ir_img = torch.split(batch['img'], split_size_or_sections=3, dim=1)
+                preds = model(rgn_img, augment=augment)
+
+            # Loss
+            with dt[2]:
+                if self.training:
+                    self.loss += model.loss(batch, preds)[1]
+
+            # Postprocess
+            with dt[3]:
+                preds = self.postprocess(preds)
+
+            self.update_metrics(preds, batch)
+            if self.args.plots and batch_i < 3:
+                self.plot_val_samples(batch, batch_i)
+                self.plot_predictions(batch, preds, batch_i)
+
+            self.run_callbacks('on_val_batch_end')
+        stats = self.get_stats()
+        self.check_stats(stats)
+        self.speed = dict(zip(self.speed.keys(), (x.t / len(self.dataloader.dataset) * 1E3 for x in dt)))
+        self.finalize_metrics()
+        self.print_results()
+        self.run_callbacks('on_val_end')
+        if self.training:
+            model.float()
+            results = {**stats, **trainer.label_loss_items(self.loss.cpu() / len(self.dataloader), prefix='val')}
+            return {k: round(float(v), 5) for k, v in results.items()}  # return results as 5 decimal place floats
+        else:
+            LOGGER.info('Speed: %.1fms preprocess, %.1fms inference, %.1fms loss, %.1fms postprocess per image' %
+                        tuple(self.speed.values()))
+            if self.args.save_json and self.jdict:
+                with open(str(self.save_dir / 'predictions.json'), 'w') as f:
+                    LOGGER.info(f'Saving {f.name}...')
+                    json.dump(self.jdict, f)  # flatten and save
+                stats = self.eval_json(stats)  # update stats
+            if self.args.plots or self.args.save_json:
+                LOGGER.info(f"Results saved to {colorstr('bold', self.save_dir)}")
+            return stats
+
+
 def val(cfg=DEFAULT_CFG, use_python=False):
     """Validate trained YOLO model on validation dataset."""
     model = cfg.model or 'yolov8n.pt'
@@ -281,31 +426,34 @@ def val(cfg=DEFAULT_CFG, use_python=False):
         from ultralytics import YOLO
         YOLO(model).val(**args)
     else:
-        validator = DetectionValidator(args=args)
+        validator = MultiModalDetectionValidator(args=args)
+
         validator(model=args['model'])
 
 
 if __name__ == '__main__':
     # val(use_python=True)
-    path_rgb = 'E:\\LLVIP\\train-visible.txt'
-    path_ir = 'E:\\LLVIP\\train-infrared.txt'
-    cfg = DEFAULT_CFG
-
-    dataset = MultiModalDataset(rgb_path=path_rgb, ir_path=path_ir,
-                                imgsz=cfg.imgsz,
-                                batch_size=cfg.batch,
-                                augment=cfg.mode == 'train',  # augmentation
-                                hyp=cfg,  # TODO: probably add a get_hyps_from_cfg function
-                                rect=cfg.rect,
-                                cache=cfg.cache or None,
-                                single_cls=cfg.single_cls or False,
-                                stride=32,
-                                pad=0.0 if cfg.mode == 'train' else 0.5,
-                                prefix=colorstr(f'{cfg.mode}: '),
-                                use_segments=cfg.task == 'segment',
-                                use_keypoints=cfg.task == 'pose',
-                                classes=cfg.classes,
-                                data=check_det_rgb_ir_dataset('../../../cfg/datasets/LLVIP.yaml'),
-                                fraction=cfg.fraction if cfg.mode == 'train' else 1.0, )
-    labels = dataset[0]
-    print(labels)
+    validator = MultiModalDetectionValidator(args=DEFAULT_CFG)
+    validator(model=DEFAULT_CFG.model)
+    # path_rgb = 'E:\\LLVIP\\train-visible.txt'
+    # path_ir = 'E:\\LLVIP\\train-infrared.txt'
+    # cfg = DEFAULT_CFG
+    #
+    # dataset = MultiModalDataset(rgb_path=path_rgb, ir_path=path_ir,
+    #                             imgsz=cfg.imgsz,
+    #                             batch_size=cfg.batch,
+    #                             augment=cfg.mode == 'train',  # augmentation
+    #                             hyp=cfg,  # TODO: probably add a get_hyps_from_cfg function
+    #                             rect=cfg.rect,
+    #                             cache=cfg.cache or None,
+    #                             single_cls=cfg.single_cls or False,
+    #                             stride=32,
+    #                             pad=0.0 if cfg.mode == 'train' else 0.5,
+    #                             prefix=colorstr(f'{cfg.mode}: '),
+    #                             use_segments=cfg.task == 'segment',
+    #                             use_keypoints=cfg.task == 'pose',
+    #                             classes=cfg.classes,
+    #                             data=check_det_rgb_ir_dataset('../../../cfg/datasets/LLVIP.yaml'),
+    #                             fraction=cfg.fraction if cfg.mode == 'train' else 1.0, )
+    # labels = dataset[0]
+    # print(labels)
