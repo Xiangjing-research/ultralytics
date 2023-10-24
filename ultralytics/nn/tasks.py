0a6ec8b3730525bf4943d3ca8fdff43ebd876383
Xiangjing-research <a1217128095@gmail.com>
2023-10-24 14:55:51 +0800

    去除_predict_augment

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -914,43 +914,5 @@ class MultispectralDetectionModel(BaseModel):
                 feature_visualization(x, m.type, m.i, save_dir=visualize)
         return x
 
-
-    def _predict_augment(self, x):
-        # """Perform augmentations on input image x and return augmented inference and train outputs."""
-        # img_size = x.shape[-2:]  # height, width
-        # s = [1, 0.83, 0.67]  # scales
-        # f = [None, 3, None]  # flips (2-ud, 3-lr)
-        # y = []  # outputs
-        # for si, fi in zip(s, f):
-        #     xi = scale_img(x.flip(fi) if fi else x, si, gs=int(self.stride.max()))
-        #     yi = super().predict(xi)[0]  # forward
-        #     yi = self._descale_pred(yi, fi, si, img_size)
-        #     y.append(yi)
-        # y = self._clip_augmented(y)  # clip augmented tails
-        # return torch.cat(y, -1), None  # augmented inference, train
-        raise NotImplementedError('Please provide _predict_augment function!')
-
-    @staticmethod
-    def _descale_pred(p, flips, scale, img_size, dim=1):
-        """De-scale predictions following augmented inference (inverse operation)."""
-        p[:, :4] /= scale  # de-scale
-        x, y, wh, cls = p.split((1, 1, 2, p.shape[dim] - 4), dim)
-        if flips == 2:
-            y = img_size[0] - y  # de-flip ud
-        elif flips == 3:
-            x = img_size[1] - x  # de-flip lr
-        return torch.cat((x, y, wh, cls), dim)
-
-    def _clip_augmented(self, y):
-        """Clip YOLOv5 augmented inference tails."""
-        nl = self.model[-1].nl  # number of detection layers (P3-P5)
-        g = sum(4 ** x for x in range(nl))  # grid points
-        e = 1  # exclude layer count
-        i = (y[0].shape[-1] // g) * sum(4 ** x for x in range(e))  # indices
-        y[0] = y[0][..., :-i]  # large
-        i = (y[-1].shape[-1] // g) * sum(4 ** (nl - 1 - x) for x in range(e))  # indices
-        y[-1] = y[-1][..., i:]  # small
-        return y
-
     def init_criterion(self):
         return v8DetectionLoss(self)
